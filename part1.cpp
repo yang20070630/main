91992e500297da649057ba0c7c2a7b2569a38a72
steven yang <3311982585@qq.com>
2020-07-19 13:18:38 +0800

    master

---
+++ part1.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int cmp1(int a,int b){
+    if(a>b)
+        return a<b;
+}
+int main()
+{
+    int a[100],n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+        cout<<a[i]<<" is go"<<endl;
+    }
+    sort(a,a+n,cmp1);
+    for (int i = 0; i < n; i++)
+    {
+        cout<<a[i]<<endl;
+    }
+    
+    return 0;
+}
